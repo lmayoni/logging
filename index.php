c838ca9576ed6f7e43c953235feeffeab4dc13df
Lucas Mayoni <lucas.mayoni@colppy.com>
2019-08-01 15:00:35 -0300

    KAN-6088 #comment es un commit v2

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $streamHandler->setFormatter($formatter);
 $logger->pushHandler($streamHandler);
 $logger->info('This is a log! ^_^ ');
 $logger->warning('This is a log warning! ^_^ ');
-$logger->error('This is a log error! ^_^ ');
+$logger->error('This is a log erro r! ^_^ ');
 
 $logger->info("Line: ".__LINE__." - Function: ".__FUNCTION__." - File: ".__FILE__." - Mensaje INFO ",array('key'=>'value'), null);
 $logger->crit("Line: ".__LINE__." - Function: ".__FUNCTION__." - File: ".__FILE__." - Mensaje INFO ",array('key'=>'value'), null);
