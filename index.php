6726d115a42652b5e8e695bd8a046c78a579b354
Lucas Mayoni <lucas.mayoni@colppy.com>
2019-08-01 14:34:58 -0300

    Se genera una prueba basica de logging.

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-<?php echo "hola";?>
+<?php
+
+require_once('vendor/autoload.php');
+
+use Monolog\Formatter\LineFormatter;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+$output = "%datetime% > %channel% > %level_name% > %message% %context% %extra%\n";
+
+$formatter = new LineFormatter($output, null, false, true);
+
+$logger = new Logger('ColppyLOG');
+$streamHandler = new StreamHandler('app.log', Logger::DEBUG);
+$streamHandler->setFormatter($formatter);
+$logger->pushHandler($streamHandler);
+$logger->info('This is a log! ^_^ ');
+$logger->warning('This is a log warning! ^_^ ');
+$logger->error('This is a log error! ^_^ ');
+
+$logger->info("Line: ".__LINE__." - Function: ".__FUNCTION__." - File: ".__FILE__." - Mensaje INFO ",array('key'=>'value'), null);
+$logger->crit("Line: ".__LINE__." - Function: ".__FUNCTION__." - File: ".__FILE__." - Mensaje INFO ",array('key'=>'value'), null);
